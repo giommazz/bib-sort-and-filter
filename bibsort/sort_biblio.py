0996b4a5e7a1364729c80eecd8984b557aeab0d2
Gabriele Iommazzo <gab.iom89@gmail.com>
2023-03-16 15:01:58 +0100

    edited python script

--- bibsort/sort_biblio.py
+++ bibsort/sort_biblio.py
@@ -8,6 +8,7 @@
 ################################################################
 
 import bibtexparser
+from bibtexparser.bwriter import BibTexWriter
 
 # Prompt the user for the input .bib file name
 input_file = input('Enter the name of the input .bib file: ')
@@ -21,18 +22,28 @@ with open(input_file) as bibtex_file:
 def sort_by_author(entry):
     author = entry.get('author', '')
     if author:
-        last_name = author.split(',')[-1].strip()
+        last_name = author.split("and")[0].split(',')[-1].strip()
+        print(last_name.lower())
         return last_name.lower()
     else:
         return ''
 
 bib_database.entries.sort(key=sort_by_author)
 
+for e in bib_database.entries:
+    print(e["author"])
+
+
 # Write the sorted entries to a new .bib file
 if not output_file:
     output_file = 'sorted_biblio.bib'
 
+# create a BibTexWriter object
+writer = BibTexWriter()
+
+# open a file for writing
 with open(output_file, 'w') as bibtex_file:
-    bibtexparser.dump(bib_database, bibtex_file)
+    # write the BibDatabase object to the file
+    bibtex_file.write(writer.write(bib_database))
 
 print('Sorted entries written to ' + output_file)
